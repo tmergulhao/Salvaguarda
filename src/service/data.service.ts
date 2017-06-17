a6a1f6355d5ce39aeba1d1348ba8ad42817ba4bf
Tiago Mergulhão <tmergulhao@users.noreply.github.com>
2017-06-17 09:28:10 -0300

    Maps and city names

--- src/service/data.service.ts
+++ src/service/data.service.ts
@@ -10,19 +10,149 @@ export class DataService{
                 img: "./../assets/img/mapa-pequeno-lago-norte.png"
             },
             {
-                nomeCidade: "Lago Norte",
+                nomeCidade: "Brasília",
                 outrasInfo: "Outras informações importantes.",
-                img: "./../assets/img/mapa-pequeno-lago-norte.png"
+                img: "./../assets/img/mapa-pequeno-brasilia.png"
             },
             {
-                nomeCidade: "Lago Norte",
+                nomeCidade: "Sudoeste",
                 outrasInfo: "Outras informações importantes.",
-                img: "./../assets/img/mapa-pequeno-lago-norte.png"
+                img: "./../assets/img/mapa-pequeno-sudoeste.png"
             },
             {
-                nomeCidade: "Lago Norte",
+                nomeCidade: "Cruzeiro",
                 outrasInfo: "Outras informações importantes.",
-                img: "./../assets/img/mapa-pequeno-lago-norte.png"
+                img: "./../assets/img/mapa-pequeno-cruzeiro.png"
+            },
+            {
+                nomeCidade: "SIA",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-sia.png"
+            },
+            {
+                nomeCidade: "Varjão",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-varjao.png"
+            },
+            {
+                nomeCidade: "Estrutural",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-estrutural.png"
+            },
+            {
+                nomeCidade: "Sobradinho",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-sobradinho.png"
+            },
+            {
+                nomeCidade: "Paranoá",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-paranoa.png"
+            },
+            {
+                nomeCidade: "Planaltina",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-planaltina.png"
+            },
+            {
+                nomeCidade: "Itapoã",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-itapoa.png"
+            },
+            {
+                nomeCidade: "Fercal",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-fercal.png"
+            },
+            {
+                nomeCidade: "Brazlândia",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-brazlandia.png"
+            },
+            {
+                nomeCidade: "São Sebastião",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-sao-sebastiao.png"
+            },
+            {
+                nomeCidade: "Santa Maria",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-santa-maria.png"
+            },
+            {
+                nomeCidade: "Lago Sul",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-lago-sul.png"
+            },
+            {
+                nomeCidade: "Jardim Botânico",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-jardim-botanico.png"
+            },
+            {
+                nomeCidade: "Gama",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-gama.png"
+            },
+            {
+                nomeCidade: "Ceilândia",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-ceilandia.png"
+            },
+            {
+                nomeCidade: "Samambaia",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-samambaia.png"
+            },
+            {
+                nomeCidade: "Taguatinga",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-taguatinga.png"
+            },
+            {
+                nomeCidade: "Recanto das Emas",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-recanto-das-emas.png"
+            },
+            {
+                nomeCidade: "Park Way",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-park-way.png"
+            },
+            {
+                nomeCidade: "Águas Claras",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-aguas-claras.png"
+            },
+            {
+                nomeCidade: "Riacho Fundo",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-riacho-fundo.png"
+            },
+            {
+                nomeCidade: "Riacho Fundo 2",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-riacho-fundo-2.png"
+            },
+            {
+                nomeCidade: "Guará",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-guara.png"
+            },
+            {
+                nomeCidade: "Vicente Pires",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-vicente-pires.png"
+            },
+            {
+                nomeCidade: "Candangolândia",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-candangolandia.png"
+            },
+            {
+                nomeCidade: "Núcleo Bandeirante",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-nucleo-bandeirante.png"
             }
         ];
     }
