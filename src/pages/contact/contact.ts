36ca9ac10bcc963df6a93ba637cfec5e71301934
Tiago Mergulhão <tmergulhao@users.noreply.github.com>
2017-06-17 09:44:51 -0300

    Linked tips data to Data Service

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { DataService } from './../../service/data.service';
+
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
-
+  dado: any;
+  constructor(public navCtrl: NavController, private data: DataService) {
+    this.dado = data.dados_policiais();  
   }
-
 }
--- src/service/data.service.ts
+++ src/service/data.service.ts
@@ -2,6 +2,15 @@ import { Injectable  } from '@angular/core';
 
 @Injectable()
 export class DataService{
+	dados_policiais(){
+		return [
+			{
+                nomeCidade: "Lago Norte",
+                outrasInfo: "Outras informações importantes.",
+                img: "./../assets/img/mapa-pequeno-lago-norte.png"
+            }
+		];
+	}
     salvos(){
         return [
             {
