20e47d947030a9666c5d7212c86922ea4e21011b
Igor Lisboa <igorlisboa34@gmail.com>
2017-07-23 18:41:10 -0300

    correcao

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-/*var express = require('express');
+var express = require('express');
 var path = require('path')
 var app = express();
 app.use(express.static(path.resolve(__dirname, "www")));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
  console.log("listening to Port", app.get("port"));
-});*/
-
+});
+/*
 var express      = require('express');
 
 var app = express()
   .use(express.static(__dirname + '/'))
-  .listen(process.env.PORT || 5000);
+  .listen(process.env.PORT || 5000);*/
